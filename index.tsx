3d8e6b362e7792e26b8a0567b34f735db95fc255
JLMP <joseleonelmorenopernia@gmail.com>
2026-01-02 16:25:34 -0400

    xfbd

--- index.tsx
+++ index.tsx
@@ -1,16 +1,1 @@
-
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("No se encontró el elemento root");
-}
-
-const root = createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Consolidado en index.html
